377c13792f238227381e116181770df07c506c55
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2021-09-25 08:42:13 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          450: "#004CFF",
+        },
+        purple: {
+          450: "#4A2EAA",
+          350: "#5F4C9F",
+        },
+        dark_blue: {
+          100: "#1B0076",
+          200: "#2C07A9",
+        },
+        red_new: "#EB175C",
+        gray_new: "#E5F1FF",
+        black_new: "#181B20",
+        blue_beach: "#F8F9FB",
+        white_new: "#F5F5F5",
+      },
+      fontFamily: {
+        archivo: ['"Archivo"', "sans-serif"],
+        inter: ['"Inter"', "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
